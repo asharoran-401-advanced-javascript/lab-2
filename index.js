cfa615fe54d46c0e1b77e56837831c0b245e5b35
asharaloran96 <asharoran96@gmail.com>
2020-01-22 07:15:40 +0200

    edit index.js

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 'use strict';
 
-const VehicleConstructor = require('./vehicle-constructor.js');
-const VehicalClass = require('./vehicle-class.js');
+const VehicleConstructor = require('./lib/vehicle-constructor.js');
+
 
 // Implement a car and motorcycle using a Constructor
 const mazda = new VehicleConstructor.Car('Mazda 3');
 console.log(mazda.name, mazda.drive(), mazda.stop());
-const mazda = new VehicalClass.Car('Mazda 3');
-console.log(mazda.name, mazda.drive(), mazda.stop());
+
 
 const harley = new VehicleConstructor.Motorcycle('Harley');
 console.log(harley.name, harley.wheelie(), harley.stop());
-const harley = new VehicalClass.Motorcycle('Harley');
-console.log(harley.name, harley.wheelie(), harley.stop());
+
 // Implement a car and motorcycle using a Class
 
+const VehicalClass = require('./lib/vehicle-class.js');
 
+const mazda = new VehicalClass.Car('Mazda 3');
+console.log(mazda.name, mazda.drive(), mazda.stop());
+
+const harley = new VehicalClass.Motorcycle('Harley');
+console.log(harley.name, harley.wheelie(), harley.stop());
